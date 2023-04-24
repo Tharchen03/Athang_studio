632ed2d63fe93be8a6433d55ccfc0bd533989a6f
Tharchen03 <sherabten098@gmail.com>
2023-04-24 12:53:42 +0600

    clone

--- resources/views/trial.blade.php
+++ resources/views/trial.blade.php
@@ -5,5 +5,5 @@
   
   <video  controls>
                    <source src="https://vimeo.com/user19468877?embedded=true&source=owner_name&owner=19468877" type="video/mp4">
-                 Your browser does not support the video tag.
+                 Your browser does not support the video tag.fdgdfg
                         </video>
