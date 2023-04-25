329c3aadab893df47f9e7fd520412fb820151586
Tharchen03 <sherabten098@gmail.com>
2023-04-25 15:02:49 +0600

    update

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -29,45 +29,20 @@
                                 <a href="/home" class="menu__link">Home</a>
                             </li>
                             <li class="menu__item">
-                                <a href="/adver" class="menu__link">Advertisement</a>
+                                <a href="/adver" class="menu__link">Ap Bokto</a>
                             </li>
                             <li class="menu__item">
-                                <a href="/details" class="menu__link">For Details</a>
+                                <a href="/details" class="menu__link">Studio</a>
                             </li>
                             <li class="menu__item">
-                                <a href="/services" class="menu__link">Our Services</a>
+                                <a href="/services" class="menu__link">Shorts</a>
                             </li>
                             <li class="menu__item">
                                 <a href="/contact" class="menu__link">Contact Us</a>
                             </li>
                         </ul>
                     </nav>
-                    <div class="header__search">
-                        <img src="search.png" alt="search" class="search__img img">
-                    </div>
-                    <a href="#" class="header__basket">
-                        <img src="basket.png" alt="bag" class="basket__img img">
-                    </a>
-                    <div class="header__social">
-                        <ul class="social__list">
-                            <li class="social__item">
-                                <a href="https://www.facebook.com/sherab.tharchen.1460" class="social__link">
-                                    <img src="fb.svg" alt="social" class="social__img img">
-                                </a>
-                            </li>
-                            <li class="social__item">
-                                <a href="https://twitter.com/home" class="social__link">
-                                    <img src="tw.svg" alt="social" class="social__img img">
-                                </a>
-                            </li>
-                            <li class="social__item">
-                                <a href="https://www.instagram.com/tharchen_03/" class="social__link">
-                                    <img src="in.svg" alt="social" class="social__img img">
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+
 
                 <div class="header__burger">
                     <span></span>
