af04420b6b042982202a2f2a1d30e69937b52e2d
Tharchen03 <sherabten098@gmail.com>
2023-04-28 10:23:29 +0600

    update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -24,14 +24,24 @@
                         <img src="Athang_logo.jpg" alt="logo" class="logo__img img">
                         <span>Athang Studio</span>
                     </a>
-                    <div class="footer__text text">
+
+                    <p class="footer-contact__item text1" style="color:#707070;!important;">Athang believes in the team
+                        work and democratic working
+                        culture to bring the best of every employees at personal as well as professional
+                        level. As credentials, Athang has been with some international
+                        organizations in IT/software Services, professional training,
+                        and 3D animations.</p>
+                    <br>
+
+
+                    <div class="footer__text text1">
                         Do follow us on
                     </div>
                     <div class="footer__socials">
-                        <a href="https://www.facebook.com/sherab.tharchen.1460" class="footer__social">
+                        <a href="https://www.facebook.com/profile.php?id=100057108984264" class="footer__social">
                             <img src="fb.svg" alt="footer img" class="footer__img img">
                         </a>
-                        <a href="https://www.instagram.com/tharchen_03/" class="footer__social">
+                        <a href="https://www.instagram.com/athang215/" class="footer__social">
                             <img src="in.svg" alt="footer img" class="footer__img img">
                         </a>
                         <a href="https://twitter.com/home" class="footer__social">
@@ -42,61 +52,65 @@
                 <div class="footer__block footer-contact">
                     <div class="footer-contact__header">Contact Us</div>
                     <ul class="footer-contact__list">
-                        <li class="footer-contact__item text">
+                        <li class="footer-contact__item text1">
                             <a href="tel:+123456789123" class="footer-contact__link">Phone: +975 02 333849/333655</a>
                         </li>
-                        <li class="footer-contact__item text">
+                        <li class="footer-contact__item text1">
                             <a href="tel:+123456789123" class="footer-contact__link">Mobile: +975 17110989/17115890</a>
                         </li>
-                        <li class="footer-contact__item text">
+                        <li class="footer-contact__item text1">
                             <a href="mailto:info@athang.com,karma@athang.com" class="footer-contact__link">Email:
                                 info@athang.com / karma@athang.com</a>
                         </li>
 
-                        <li class="footer-contact__item text">
+                        <li class="footer-contact__item text1">
                             <a href="https://www.google.com/maps/place/Athang+Learning+Institute/@27.4712215,89.6290332,17z/data=!3m1!4b1!4m11!1m3!11m2!2shsCfYudIceHKNGF2RMSX9M272f76FA!3e1!3m6!1s0x39e1958b0fdbbf2f:0x6ad2ba8a60502b3e!8m2!3d27.4712216!4d89.6339041!15sCgEqkgEPdHJhaW5pbmdfc2Nob29s4AEA!16s%2Fg%2F11fq0r_qdk"
-                                class="footer-contact__link" target="_blank">Babena, Below Dechen Phodrang Monastery.</a>
+                                class="footer-contact__link" target="_blank">Babena, Below Dechen Phodrang
+                                Monastery.</a>
                         </li>
                     </ul>
                 </div>
                 <div class="footer__block footer-link">
                     <div class="footer-link__header">Explore</div>
                     <ul class="footer-link__list">
-                        <li class="footer-link__item text">
+                        <li class="footer-link__item text1">
                             <a href="#" class="footer-link__link">About Us</a>
                         </li>
-                        <li class="footer-link__item text">
-                            <a href="#" class="footer-link__link">Advertisement</a>
+                        <li class="footer-link__item text1">
+                            <a href="/adver" class="footer-link__link">Ap Bokto Series</a>
                         </li>
-                        <li class="footer-link__item text">
-                            <a href="#" class="footer-link__link">Contact Us</a>
+
+                        <li class="footer-link__item text1">
+                            <a href="/video" class="footer-link__link">Shorts</a>
                         </li>
-                        <li class="footer-link__item text">
-                            <a href="#" class="footer-link__link">Privacy Policy</a>
+                        <li class="footer-link__item text1">
+                            <a href="/contact" class="footer-link__link">Contact Us</a>
                         </li>
+
                     </ul>
                 </div>
                 <div class="footer__block footer-link">
                     <div class="footer-link__header">Useful links</div>
                     <ul class="footer-link__list">
-                        <li class="footer-link__item text">
+                        <li class="footer-link__item text1">
                             <a href="https://www.athang.com/public/w/aboutus" class="footer-link__link">About Athang</a>
                         </li>
-                        <li class="footer-link__item text">
-                            <a href="#" class="footer-link__link">Industrial Services</a>
+                        <li class="footer-link__item text1">
+                            <a href="https://www.athang.com/public/ppage" class="footer-link__link">News &
+                                Announcements</a>
                         </li>
-                        <li class="footer-link__item text">
-                            <a href="#" class="footer-link__link">Events & Exhibition</a>
+                        <li class="footer-link__item text1">
+                            <a href="#" class="footer-link__link"></a>
                         </li>
-                        <li class="footer-link__item text">
-                            <a href="#" class="footer-link__link">Hospital Catering</a>
+                        <li class="footer-link__item text1">
+                            <a href="#" class="footer-link__link"></a>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="footer__copyright">
-                <div class="footer__cooper text">Copyright ©
-                    <a href="https://dimafomaa.online" class="footer__link-bio" target="_blank">tharchen_03</a>, 2023.
+                <div class="footer__cooper text1">Copyright ©
+                    <a href="https://github.com/Tharchen03/Athang_studio" class="footer__link-bio" target="_blank">Athang_Studio</a>, 2023.
                     All rights reserved.
                 </div>
             </div>
