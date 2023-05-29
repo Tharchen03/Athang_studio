f2e20bfb65b78d1a77da5d29a04fcea869a403ea
sumansunar <sinaeve077@gmail.com>
2023-05-29 20:38:37 +0600

    new

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,7 @@
             @yield('content')
         </div>
 
-        <section class="contact" id="contact">
+        <!-- <section class="contact" id="contact">
             <div class="container">
                 <div class="contact__inner">
                     <h2 class="contact__title">Contact Us</h2>
@@ -37,7 +37,50 @@
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3538.947786856308!2d89.62837367589461!3d27.501999234517804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e1943d9df45cc3%3A0x14944eaf62ce3e40!2sAthang%20Training%20Academy!5e0!3m2!1sen!2sbt!4v1682394505340!5m2!1sen!2sbt"
                 width="1840" height="450" style="border:0;" allowfullscreen="" loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"></iframe></p>
-        <br>
+        <br> -->
+
+<!-- ======= Contact Section ======= -->
+<section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h1>Contact us</h1>
+          
+        </div>
+<br>
+        <div class="row">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Babna</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@athang.com / karma@athang.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+975 02 333849/333655</p>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3538.947786856308!2d89.62837367589461!3d27.501999234517804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e1943d9df45cc3%3A0x14944eaf62ce3e40!2sAthang%20Training%20Academy!5e0!3m2!1sen!2sbt!4v1682394505340!5m2!1sen!2sbt" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          
+
+      </div>
+    </section><!-- End Contact Section -->
+
+
         @include('footer')
 
 
